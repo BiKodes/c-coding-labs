7dda5da4f81ea2125758a073917bde554a7d4857
biko <steven.biko@savannahinformatics.com>
2024-10-08 06:08:14 +0300

    feat: programming in C symbolic constants

--- getting_started/for_statement.c
+++ getting_started/for_statement.c
@@ -2,9 +2,9 @@
 
 /* print Fahrenheit-Celsius */
 
-main()
-{
-    int fahr;
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-}
+float main()
+    {
+        int fahr;
+        for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+            printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
